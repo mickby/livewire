d55218017215a683be9a2497b5263e963bdde8f2
mick <mickb.py@gmail.com>
2025-09-08 12:45:05 +0100

    End of episode 2

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -4,9 +4,17 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+
+
 class Greeter extends Component
 {
     public $name = "Mick";
+
+    public function changeName($newName)
+    {
+        $this->name = $newName;
+    }
+
     public function render()
     {
         return view('livewire.greeter');
