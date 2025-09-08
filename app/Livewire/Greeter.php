b28f21102789f099a543b50fa282082cf13b2d7a
mick <mickb.py@gmail.com>
2025-09-08 14:33:38 +0100

    Just started Episode 6

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -2,19 +2,45 @@
 
 namespace App\Livewire;
 
+use App\Models\Greeting;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
-
-
 
 class Greeter extends Component
 {
-    public $name ='';
-    public $greeting ='';
+    #[Validate('required|min:2')]
+    public $name = '';
+    public $greeting = '';
+    public $greetings = [];
+    public $greetingMessage = '';
 
-    public function changeName()
-    {
+    public function changeGreeting() {
+        $this->reset('greetingMessage');
 
+        $this->validate();
+
+        $this->greetingMessage = "{$this->greeting}, {$this->name}!";
     }
+
+    public function mount() {
+        $this->greetings = Greeting::all();
+    }
+
+    public function updated($property, $value) {
+        // if ($property === 'name') {
+        //     $this->name = strtolower($value);
+        // }
+    }
+
+    public function updatedName($value) {
+        $this->name = strtolower($value);
+    }
+
+    // public function rules() {
+    //     return [
+    //         'name' => 'required|min:2',
+    //     ];
+    // }
 
     public function render()
     {
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,43 +1,42 @@
-
 <div>
-
     <form
-        wire:submit="changeName()">
+        wire:submit="changeGreeting()"
+    >
         <div class="mt-2">
             <select
                 type="text"
                 class="p-4 border rounded-md bg-gray-700 text-white"
                 wire:model.fill="greeting"
-                >
-                <option value="Hello">Hello</option>
-                <option value="Hi">Hi</option>
-                <option value="Hey">Hey</option>
-                <option selected value="Greetings">Greetings</option>
+            >
+                @foreach ($greetings as $item)
+                    <option value="{{$item->greeting}}">
+                        {{$item->greeting}}
+                    </option>
+                @endforeach
             </select>
-
-
             <input
                 type="text"
                 class="p-4 border rounded-md bg-gray-700 text-white"
-                wire:model.change="name"
+                wire:model="name"
             >
         </div>
-
+        <div>
+            @error('name')
+            {{$message}}
+            @enderror
+        </div>
         <div class="mt-2">
             <button
                 type="submit"
-                class="text-white font-medium rounded.md px-4 py-2 bg-blue-600"
+                class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
             >
                 Greet
             </button>
-
         </div>
     </form>
-
-    @if ($name !='')
-        <div class="mt-4">
-            {{$greeting}}, {{ $name  }}!
+    @if ($greetingMessage !== '')
+        <div>
+            {{$greetingMessage}}
         </div>
     @endif
-
 </div>
