5b672d189403d2463f4bf246277bd84117c446a0
mick <mickb.py@gmail.com>
2025-09-08 16:19:07 +0100

    Just started Episode 8

---
+++ resources/views/livewire/show-article.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h2 class="text-2xl  font-bold mb-4">
+        {{$article->title}}
+    </h2>
+    <div class="mt-4">
+        {{ $article->content}}
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Livewire\Search;
+use App\Livewire\ShowArticle;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/search', Search::class)->name('search');
+
+Route::get('/articles/{article}', ShowArticle::class)->name('show');
+
+
