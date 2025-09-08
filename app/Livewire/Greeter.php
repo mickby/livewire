cf712011e44699da29d4ad6067a13108d1d57946
mick <mickb.py@gmail.com>
2025-09-08 12:02:20 +0100

    initial commit

---
+++ app/Livewire/Greeter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Greeter extends Component
+{
+    public $name = "Mick";
+    public function render()
+    {
+        return view('livewire.greeter');
+    }
+}
